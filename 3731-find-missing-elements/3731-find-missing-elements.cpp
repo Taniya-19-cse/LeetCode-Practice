29c2bddc97d9140a3d700b0e1cf9e8ac48612662
Taniya-19-cse <taniyakashyap2005@gmail.com>
2026-08-04 20:02:37 +0530

    Time: 0 ms (100%), Space: 32.2 MB (75.54%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -3,19 +3,14 @@ public:
     vector<int> findMissingElements(vector<int>& nums) {
         vector<int> ans;
         sort(nums.begin(),nums.end());
+        int j=0;
         for(int i=nums[0];i<=nums[nums.size()-1];i++){
-            int cnt=0;
-            for(int j=0;j<nums.size();j++)
-            {
-                if(nums[j]==i)
-                {
-                    cnt++;
-                }
-            }
-            if(cnt==0)
-            {
-                ans.push_back(i);
-            }
+           if(nums[j]!=i)
+           {
+            ans.push_back(i);
+           }else{
+            j++;
+           }
         }
         return ans;
     }
